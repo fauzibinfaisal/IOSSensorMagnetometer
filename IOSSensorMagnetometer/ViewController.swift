2b29f204a55c250a1bdab228d91724f376cc85c1
Fauzi <fauzibinfaisal@gmail.com>
2019-07-07 06:39:47 +0700

    gitignore

--- IOSSensorMagnetometer/ViewController.swift
+++ IOSSensorMagnetometer/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var valueXLabel: UILabel!
+    @IBOutlet weak var valueYLabel: UILabel!
+    @IBOutlet weak var valueZLabel: UILabel!
+    
+    //TODO 1: Initialize CMMotionManager
+    let motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        startMagnetometer()
+    }
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        stopMagnetometer()
+    }
+    
+    func startMagnetometer() {
+        if motionManager.isMagnetometerAvailable {
+            motionManager.magnetometerUpdateInterval = 0.1
+            
+            //Double click pada handlernya
+            motionManager.startMagnetometerUpdates(to: OperationQueue.main) { (data, error) in
+                print("magnetometerData \(String(describing: data))")
+                
+                self.valueXLabel.text = String(format: "%.2f",((data?.magneticField.x)!))
+                self.valueYLabel.text = String(format: "%.2f",((data?.magneticField.y)!))
+                self.valueZLabel.text = String(format: "%.2f",((data?.magneticField.z)!))
+            }
+        }
+    }
+    
+    func stopMagnetometer() {
+        motionManager.stopMagnetometerUpdates()
     }
 
 
 }
+
 
