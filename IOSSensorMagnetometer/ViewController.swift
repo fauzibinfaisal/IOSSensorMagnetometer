471b9120881c839e6e671ff93b8a1cb87555f985
Fauzi <fauzibinfaisal@gmail.com>
2019-07-11 13:33:43 +0700

    change to detect metal

--- IOSSensorMagnetometer/ViewController.swift
+++ IOSSensorMagnetometer/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var valueXLabel: UILabel!
     @IBOutlet weak var valueYLabel: UILabel!
-    @IBOutlet weak var directionLabel: UILabel!
+    @IBOutlet weak var valueZLabel: UILabel!
+    @IBOutlet weak var colorView: UIView!
     
     //TODO 1: Initialize CMMotionManager
     let motionManager = CMMotionManager()
@@ -21,7 +22,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     }
     
     @IBAction func startButton(_ sender: UIButton) {
@@ -57,21 +57,37 @@ class ViewController: UIViewController {
 //
 //                }
                 
-                if (((data!.magneticField.x > 4)&&(data!.magneticField.x < 9))&&((data!.magneticField.y > 29)&&(data!.magneticField.y < 34))){
-                    self.directionLabel.text = "heading to NORTH"
-                } else if (((data!.magneticField.x > -30)&&(data!.magneticField.x < -25))&&((data!.magneticField.y > -8)&&(data!.magneticField.y < -3))){
-                    self.directionLabel.text = "heading to EAST"
-                } else if (((data!.magneticField.x > 5)&&(data!.magneticField.x < 10))&&((data!.magneticField.y > -45)&&(data!.magneticField.y < -40))){
-                    self.directionLabel.text = "heading to SOUTH"
-                } else if (((data!.magneticField.x > 45)&&(data!.magneticField.x < 50))&&((data!.magneticField.y > -10)&&(data!.magneticField.y < 1))){
-                    self.directionLabel.text = "heading to WEST"
+//                if (((data!.magneticField.x > 25)&&(data!.magneticField.x < 30))&&((data!.magneticField.y > 22)&&(data!.magneticField.y < 27))){
+//                    self.directionLabel.text = "heading to NORTH"
+//                } else if (((data!.magneticField.x > -17)&&(data!.magneticField.x < -12))&&((data!.magneticField.y > 0)&&(data!.magneticField.y < 5 ))){
+//                    self.directionLabel.text = "heading to EAST"
+//                } else if (((data!.magneticField.x > 5)&&(data!.magneticField.x < 10))&&((data!.magneticField.y > -45)&&(data!.magneticField.y < -40))){
+//                    self.directionLabel.text = "heading to SOUTH"
+//                } else if (((data!.magneticField.x > 50)&&(data!.magneticField.x < 55))&&((data!.magneticField.y > 0)&&(data!.magneticField.y < 5))){
+//                    self.directionLabel.text = "heading to WEST"
+//                } else{
+//                    self.directionLabel.text = "Where am I ?"
+//                }
+                
+                
+                //In the magnetometer code, you would use a distance heuristic to sense a large change in the magnetic field.
+                
+                let totalMagneticField = abs(data!.magneticField.x)+abs(data!.magneticField.y)+abs(data!.magneticField.z)
+                
+
+                if (totalMagneticField > 1000){
+                    
+                    let y  = CGFloat(data!.magneticField.y)/2000
+                    self.colorView.backgroundColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: y)
+                    self.valueYLabel.text = "I SENSE SOMETHING"
                 } else{
-                    self.directionLabel.text = "Where am I ?"
+                    self.valueYLabel.text = "ALL CLEAR"
+                    self.colorView.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha:1)
                 }
                 
-                
                 self.valueXLabel.text = String(format: "%.2f",((data?.magneticField.x)!))
-                self.valueYLabel.text = String(format: "%.2f",((data?.magneticField.y)!))
+
+
             }
         }
     }
